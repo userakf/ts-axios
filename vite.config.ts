42f80dce51ca80fe9a45ef3aa1871d3fb9f6d4f5
userakf <820279897@qq.com>
2022-09-22 22:16:40 +0800

    chore: update

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import Vue from '@vitejs/plugin-vue'
 export default defineConfig({
     plugins: [
-        vue()
+        Vue()
     ]
 })
